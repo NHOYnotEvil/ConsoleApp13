663a6bffcdb41fdd7f229bc8d37bf32ca941054c
NHOY <NHOY@2210DR000919>
2023-05-04 00:44:48 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Кузнецов";
+            string familia = "Василий";
+            Console.WriteLine("Ваше имя: " + name + ". Ваша фамилия: " + familia + ".");
+            Console.WriteLine("Ваше имя: " + familia + ". Ваша фамилия: " + name + ".");
+        }
+    }
+}
